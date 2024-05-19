d67b472f845ad934238c9814b0a7d8affbdd044c
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-19 14:07:51 +0000

    feat: added checker

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,43 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      const {puzzle, coordinate, value} = req.body;
+      if (!puzzle | !coordinate | !value) {
+        res.json({error: "Required field(s) missing"});
+        return;
+      }
+
+      const message = solver.validate(puzzle);
+      if (message) {
+        res.json({error: message});
+        return;
+      }
+
+      const coordinateRegex = /^[A-I][1-9]$/;
+      const valueRegex = /^[1-9]$/;
+      if (!coordinateRegex.test(coordinate)) {
+        res.json({error: "Invalid coordinate"});
+        return;
+      } else if (!valueRegex.test(value)) {
+        res.json({error: "Invalid value"});
+        return;
+      }
+
+      const row = coordinate[0];
+      const column = coordinate[1];
+
+      const rowCheck = solver.checkRowPlacement(puzzle, row, column, value);
+      const colCheck = solver.checkColPlacement(puzzle, row, column, value);
+      const regionCheck = solver.checkRegionPlacement(puzzle, row, column, value);
+      let conflict = [];
+      conflict.push(rowCheck[1], colCheck[1], regionCheck[1]);
+      conflict = conflict.filter((conflict) => conflict !== "");
+
+      if (conflict.length !== 0) {
+        res.json({valid: false, conflict});
+      } else {
+        res.json({valid: true});
+      }
 
     });
     
